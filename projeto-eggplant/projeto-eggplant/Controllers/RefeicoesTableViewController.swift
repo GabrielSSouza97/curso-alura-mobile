7f112e8f28957c3d7901ffeed068fb8f0b523a7b
GabrielSSouza97 <gabriel.s.souza9755@gmail.com>
2020-12-23 15:37:35 -0300

    Módulo 1.3.2
    Utilizando alerts e mostrando detalhes das refeições com o longPress

--- projeto-eggplant/projeto-eggplant/Controllers/RefeicoesTableViewController.swift
+++ projeto-eggplant/projeto-eggplant/Controllers/RefeicoesTableViewController.swift
@@ -37,13 +37,15 @@ class RefeicoesTableViewController: UITableViewController, AdicionaRefeicaoDeleg
     @objc func mostrarDetalhes(_ gesture: UILongPressGestureRecognizer) {
         if gesture.state == .began {
             let celula = gesture.view as! UITableViewCell
-            
             guard let indexPath = tableView.indexPath(for: celula) else {
                 return
             }
-            
             let refeicao = refeicoes[indexPath.row]
-            print("Refeição: \(refeicao.nome)")
+            
+            let alerta = UIAlertController(title: refeicao.nome, message: refeicao.detalhes(), preferredStyle: .alert)
+            let botaoCancelar = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+            alerta.addAction(botaoCancelar)
+            present(alerta, animated: true, completion: nil)
         }
     }
     
